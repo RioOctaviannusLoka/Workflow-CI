27f03b977d634b70d98846126bbc00d3026b97ec
Rio <rioloka86@gmail.com>
2025-08-13 09:16:13 +0700

    feat: MLProject done

---
+++ MLProject/modelling.py
@@ -0,0 +1,23 @@
+import mlflow
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_experiment("Diabetes Prediction")
+
+df_train = pd.read_csv("./diabetes_prediction_preprocessing/diabetes_train.csv")
+df_test = pd.read_csv("./diabetes_prediction_preprocessing/diabetes_test.csv")
+
+X_train = df_train.drop(columns=['diabetes'])
+y_train = df_train['diabetes']
+
+X_test = df_test.drop(columns=['diabetes'])
+y_test = df_test['diabetes']
+
+with mlflow.start_run():
+    mlflow.autolog()
+
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    
+    accuracy = model.score(X_test, y_test)
