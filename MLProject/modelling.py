f5fd330c4175f5de9eb8fe1319ec1ae4832164fb
Rio Octaviannus Loka <114414891+RioOctaviannusLoka@users.noreply.github.com>
2025-08-13 09:53:57 +0700

    fix: Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,6 +2,8 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
+mlflow.autolog()
+
 df_train = pd.read_csv("./diabetes_prediction_preprocessing/diabetes_train.csv")
 df_test = pd.read_csv("./diabetes_prediction_preprocessing/diabetes_test.csv")
 
@@ -11,10 +13,7 @@ y_train = df_train['diabetes']
 X_test = df_test.drop(columns=['diabetes'])
 y_test = df_test['diabetes']
 
-with mlflow.start_run():
-    mlflow.autolog()
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
 
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-    
-    accuracy = model.score(X_test, y_test)
+accuracy = model.score(X_test, y_test)
